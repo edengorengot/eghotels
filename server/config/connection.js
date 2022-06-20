65485f5feece0320fcdc2bc8ba71dfa1bb863d36
edengorengot <eden.gor@gmail.com>
2022-06-20 10:36:21 +0300

    V0.0.0.2 - changes to express and cleaning

---
+++ server/config/connection.js
@@ -0,0 +1,25 @@
+/*=== main connection file ===*/
+/* Module dependencies */
+const dotenv = require('dotenv').config();
+const app = require('../app');
+// const http = require('http');
+const mongoose = require('mongoose');
+const chalk = require('chalk');
+
+
+/* Connect to Database MongoDB */
+mongoose.connect(process.env.MONGODB)
+.then(() => {
+    console.log(chalk.greenBright("connected to mongoDB"));
+})
+.catch(() => {
+    console.log(chalk.red("could not connect to mongoDB"));
+});
+
+
+/* Connect to Port */
+const PORT = process.env.PORT || 3007;
+
+app.listen(PORT, () => {
+  console.log(chalk.yellow(`The server is listening to port: ${PORT} || Link: http://localhost:${PORT}/`));
+});
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log('The basic GET method works!');
+  res.json({ msg: "im Here" });
+  // res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
